fadfffaad14a1c124dc48d6eb0aa8df36e1535a2
Jason Anton <rev.jt.anton@gmail.com>
2019-10-11 08:02:15 -0400

    making it even more paired down by removing controllers

--- app/app.js
+++ app/app.js
@@ -3,9 +3,7 @@ var bodyParser = require("body-parser");
 var nunjucks = require('nunjucks');
 var path = require('path');
 var app = express();
-var Home = require('./controllers/Home');
-var Second = require('./controllers/Second');
-var Third = require('./controllers/Third');
+var View = require("./views/Base");
 
 app.set('views', __dirname + '/templates');
 app.set('view engine', 'njk');
@@ -13,21 +11,24 @@ app.use("/public", express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-nunjucks.configure(['app/templates','app/templates/instruction-forms','app/templates/instruction-results'], {
+nunjucks.configure(['app/templates', 'app/templates/instruction-forms', 'app/templates/instruction-results'], {
   autoescape: true,
   express: app
 });
 
-app.get('/', function(req, res) {
-  Home.run(req, res);
+app.get('/', function (req, res) {
+  var v = new View(res, 'home');
+  v.render();
 });
 
-app.post('/second', function(req, res) {
-  Second.run(req, res);
+app.post('/second', function (req, res) {
+  var v = new View(res, 'second');
+  v.render(req.body);
 });
 
-app.post('/third', function(req, res) {
-  Third.run(req,res);
+app.post('/third', function (req, res) {
+  var v = new View(res, 'third');
+  v.render(req.body);
 })
- 
+
 module.exports = app;
