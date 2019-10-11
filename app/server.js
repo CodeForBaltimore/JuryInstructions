ffce6acfcb584ea71d4eda5c1e441b228519cc98
Jason Anton <rev.jt.anton@gmail.com>
2019-10-10 21:06:45 -0400

    changing the port number for heroku

--- app/server.js
+++ app/server.js
@@ -1,5 +1,5 @@
 var app = require("./app");
 
-app.listen(4000, function () {
-    console.log('App listening on port 4000!');
+app.listen(3000, function () {
+    console.log('App listening on port 3000!');
   });
